6b8bb211f3a84ee9f64b86cf1288a47eee0ba27d
unknown <Andrew.mkoch101@gmail.com>
2026-08-29 03:01:56 +1000

    Created URL patterns

--- djangoProject/invoiceAPI/invoices/urls.py
+++ djangoProject/invoiceAPI/invoices/urls.py
@@ -3,11 +3,10 @@ from django.urls import path
 
 from rest_framework.routers import DefaultRouter
 
-from invoiceAPI.invoices.models import InvoiceItem
-from views import (CustomerViewSet, InvoiceViewSet, InvoiceItemViewSet, PaymentViewSet)
+from .views import (CustomerViewSet, InvoiceViewSet, InvoiceItemViewSet, PaymentViewSet)
 
 #Create and configure URL patterns:
-router = DefaultRouter
+router = DefaultRouter()
 router.register("customers", CustomerViewSet)
 router.register("invoices", InvoiceViewSet)
 router.register("invoice-items", InvoiceItemViewSet)
