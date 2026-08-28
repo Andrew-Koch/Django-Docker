44e2d34f3623724962fb84e479e46d9902e99d62
unknown <Andrew.mkoch101@gmail.com>
2026-08-29 02:44:49 +1000

    Created REST viewsets

--- djangoProject/invoiceAPI/invoices/views.py
+++ djangoProject/invoiceAPI/invoices/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Customer, Invoice, InvoiceItem, Payment
+from .serializers import (CustomerSerializer, InvoiceSerializer, InvoiceItemSerializer, PaymentSerializer,
+InvoiceSerializer, InvoiceItemSerializer, PaymentSerializer)
 
 # Create your views here.
+
+#Add CRUD functionality to Customer model class:
+class CustomerViewSet(viewsets.ModelViewSet):
+    queryset = Customer.objects.all()
+    serializer_class = InvoiceSerializer
+
+#Add CRUD functionality to Invoice model class:
+class InvoiceViewSet(viewsets.ModelViewSet):
+    queryset = Invoice.objects.all()
+    serializer_class = InvoiceSerializer
+
+#Add CRUD functionality to InvoiceItem model class:
+class InvoiceItemViewSet(viewsets.ModelViewSet):
+    queryset = InvoiceItem.objects.all()
+    serializer_class = InvoiceSerializer
+
+#Add CRUD functionality to Payment model class:
+class PaymentViewSet(viewsets.ModelViewSet):
+    queryset = Payment.objects.all()
+    serializer_class = InvoiceSerializer
